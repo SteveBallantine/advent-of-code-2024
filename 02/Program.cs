14dcc0c9887af468dfd837076ecfa1bf1ec6b2c1
steveballantine <steve.ballantine@gearset.com>
2024-12-02 07:27:51 +0000

    Day 2 Part 2

--- 02/Program.cs
+++ 02/Program.cs
@@ -9,18 +9,19 @@ var exampleInput = @"7 6 4 2 1
 8 6 4 4 1
 1 3 6 7 9";
 AssertFor(exampleInput, false, 2);
+AssertFor(exampleInput, true, 4);
 
 Console.WriteLine("Part1:");
 Console.WriteLine(RunFor(File.ReadAllLines(@"/Users/steveballantine/RiderProjects/advent-of-code-2024/02/input.txt"), false, false));
 
-//Console.WriteLine("Part2:");
-//Console.WriteLine(RunFor(File.ReadAllLines(@"/Users/steveballantine/RiderProjects/advent-of-code-2024/02/input.txt"), true, false));
+Console.WriteLine("Part2:");
+Console.WriteLine(RunFor(File.ReadAllLines(@"/Users/steveballantine/RiderProjects/advent-of-code-2024/02/input.txt"), true, false));
 
 
 long RunFor(string[] input, bool part2, bool logging)
 {
     var result = input.Select(i => i.Split(' ').Select(int.Parse).ToArray())
-        .Count(i => IsSafe(i));
+        .Count(i => IsSafe(i, part2, logging));
     
     return result;
 }
@@ -28,7 +29,8 @@ long RunFor(string[] input, bool part2, bool logging)
 void AssertFor(string input, bool part2, long expectedResult)
 {
     var lines = input.Split(System.Environment.NewLine);
-    var result = RunFor(lines, part2, true);
+    var result = RunFor(lines, part2, false);
+    
     if (result != expectedResult)
     {
         foreach (var line in lines)
@@ -39,28 +41,54 @@ void AssertFor(string input, bool part2, long expectedResult)
     }
 }
 
-bool IsSafe(int[] report)
+bool IsSafe(int[] report, bool part2, bool logging)
+{
+    var result = IsReportSafe(report, logging);
+
+    if (!result && part2)
+    {
+        var removingMakesSafe = false;
+        var i = 0;
+        while(!removingMakesSafe && i < report.Length)
+        {
+            var list = report.ToList();
+            list.RemoveAt(i);
+            removingMakesSafe = removingMakesSafe || IsReportSafe(list.ToArray(), logging);
+            i++;
+        }
+
+        result = removingMakesSafe;
+    }
+
+    if (logging)
+    {
+        Console.WriteLine(string.Join(',', report));
+        Console.WriteLine(result ? "SAFE" : "NOT SAFE");
+    }
+
+    return result;
+}
+
+
+bool IsReportSafe(int[] report, bool logging)
 {
     bool result = true;
-    bool increasing = true;
+    bool increasing = report[0] < report[1];
     int i = 0;
     
     while (result && i < report.Length - 1)
     {
-        var diff = report[i] - report[i + 1];
-        if (i == 0)
-        {
-            increasing = diff > 0;
-        }
-        else
-        {
-            result = result && increasing ? diff > 0 : diff < 0;
-        }
-        result = result && diff >= -3 && diff <= 3 && diff != 0;
+        result = Compare(report[i], report[i + 1], increasing);
         i++;
     }
-    Console.WriteLine(string.Join(',', report));
-    Console.WriteLine(result ? "SAFE" : "NOT SAFE");
     
+    return result;
+}
+
+bool Compare(int i1, int i2, bool increasing)
+{
+    var diff = i1 - i2;
+    var result = increasing ? diff < 0 : diff > 0;
+    result = result && diff >= -3 && diff <= 3 && diff != 0;
     return result;
 }
